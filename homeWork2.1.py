9be41438616d109ff02fd5e612e0693ecc3a0f3d
VitalyPerepelko <Perepelkovitaly@gmail.com>
2021-04-23 18:49:22 +0300

    123

--- homeWork2.1.py
+++
@@ -1,8 +0,0 @@
-#Vitaly Perepelko
-#home_work2_3
-
-print(type(15 * 3))
-print(type(15 / 3))
-print(type(15 // 3))
-print(type(15 ** 3))
-
--- homeWork2.4.py
+++
@@ -1,7 +0,0 @@
-#Vitaly Perepelko
-#home_work2_4
-
-bad_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА',
-            'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for position in bad_list:
-    print('Привет', position.split()[-1].title())
